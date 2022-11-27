bd573edf2306257e1be1a5a1361e8d70991b8006
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-11-27 17:16:46 -0300

    setup new meta title

--- src/root.tsx
+++ src/root.tsx
@@ -20,7 +20,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>Welcome to Moden - Find a weather based on its location</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
