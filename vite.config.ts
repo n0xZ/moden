7c865756fb18fc40b54bb68592db7f55f2cafb27
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-11-27 12:22:12 -0300

    set netlify adapter on vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
+import Netlify from 'solid-start-netlify'
 import Unocss from 'unocss/vite'
 import { presetUno } from 'unocss'
 export default defineConfig({
-	plugins: [solid({ ssr: false }), Unocss({ presets: [presetUno()] })],
+	plugins: [
+		solid({ ssr: true, adapter: Netlify({ edge: true }) }),
+		Unocss({ presets: [presetUno()] }),
+	],
 })
