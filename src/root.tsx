3bb00479371828f9ce03070f1153b09e96a6967a
n0xZ <o.gonzalo@hotmail.com>
2022-11-27 23:06:15 -0300

    setup global styles on root component

--- src/root.tsx
+++ src/root.tsx
@@ -1,41 +1,39 @@
 // @refresh reload
-import { Suspense } from "solid-js";
+import { Suspense } from 'solid-js'
 import {
-  A,
-  Body,
-  ErrorBoundary,
-  FileRoutes,
-  Head,
-  Html,
-  Meta,
-  Routes,
-  Scripts,
-  Title,
-} from "solid-start";
+	A,
+	Body,
+	ErrorBoundary,
+	FileRoutes,
+	Head,
+	Html,
+	Meta,
+	Routes,
+	Scripts,
+	Title,
+} from 'solid-start'
 
 import '@unocss/reset/antfu.css'
 import 'virtual:uno.css'
 
 export default function Root() {
-  return (
-    <Html lang="en">
-      <Head>
-        <Title>Welcome to Moden - Find a weather based on its location</Title>
-        <Meta charset="utf-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Body>
-        <Suspense>
-          <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
-            <Routes>
-              <FileRoutes />
-            </Routes>
-          </ErrorBoundary>
-        </Suspense>
-        <Scripts />
-      </Body>
-    </Html>
-  );
+	return (
+		<Html lang="en">
+			<Head>
+				<Title>Welcome to Moden - Find a weather based on its location</Title>
+				<Meta charset="utf-8" />
+				<Meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<Body class='bg-pink-50 c-neutral-700'>
+				<Suspense>
+					<ErrorBoundary>
+						<Routes>
+							<FileRoutes />
+						</Routes>
+					</ErrorBoundary>
+				</Suspense>
+				<Scripts />
+			</Body>
+		</Html>
+	)
 }
