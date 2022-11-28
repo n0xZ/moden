0b6ddc08a81c6f5857abc24e18473560eaaa2135
n0xZ <o.gonzalo@hotmail.com>
2022-11-27 23:07:00 -0300

    chore: setup weather location component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
-import { ErrorBoundary, Show } from 'solid-js'
+import { ErrorBoundary, Match, Show, Switch } from 'solid-js'
 import { useRouteData } from 'solid-start'
 import { createServerAction$, createServerData$ } from 'solid-start/server'
+import WeatherResult from '~/components/weather-result'
 
 import { Weather } from '~/types'
 
@@ -27,7 +28,7 @@ export default function Home() {
 		const API_URL = import.meta.env.VITE_API_URL
 		const RAPIDAPI_HOST = import.meta.env.VITE_RAPID_API_HOST
 		const RAPIDAPI_KEY = import.meta.env.VITE_RAPID_API_KEY
-		console.log(RAPIDAPI_HOST)
+
 		const res = await fetch(`${API_URL}?q=${location}`, {
 			headers: {
 				'X-RapidAPI-Key': RAPIDAPI_KEY,
@@ -41,33 +42,46 @@ export default function Home() {
 		<ErrorBoundary
 			fallback={<div>An error has ocurred while displaying the content.</div>}
 		>
-			<main class="h-screen w-full flex flex-col justify-center container mx-auto max-w-3xl">
-				<Form class="flex flex-col justify-center space-y-3 max-w-3xl xl:p-0 p-2">
-					<label class="font-semibold" for="location">
-						Your location
-					</label>
-					<input
-						class="px-4 py-3 rounded-lg outline-none border-2 max-w-3xl border-light-500 "
-						type="text"
-						name="location"
-						placeholder="I.E Buenos Aires"
-						disabled={action.pending}
-					/>
-					<button
-						class="px-4 py-3 rounded-lg max-w-3xl w-full bg-amber-300 c-neutral-800  font-semibold"
-						type="submit"
-						disabled={action.pending}
-					>
-						Search weather based on your location
-					</button>
-				</Form>
-				<Show when={action.result}>
-					<p>
-						Actual condition : {action.result.current.cloud} on{' '}
-						{action.result.location.name}
-					</p>
-					<p>{action.result.location.country}</p>
-				</Show>
+			<main class="h-full  ">
+				<section class="h-screen w-full flex flex-col justify-center container mx-auto max-w-3xl ">
+					<h1 class="text-center text-xl">
+						Find your weather based on your location!
+					</h1>
+					<Form class="flex flex-col justify-center space-y-3 max-w-3xl xl:p-0 p-2">
+						<label class="font-semibold" for="location">
+							Your location
+						</label>
+						<input
+							class="px-4 py-3 rounded-lg outline-none border-2 max-w-3xl border-light-400 bg-sky-50"
+							type="text"
+							name="location"
+							placeholder="I.E Buenos Aires"
+							required
+							disabled={action.pending}
+						/>
+						<button
+							class={`px-4 py-3 rounded-lg max-w-3xl w-full ${
+								action.pending ? 'bg-pink-500' : 'bg-pink-300'
+							}   font-semibold hover:bg-pink-400 duration-100 ease-in-out`}
+							type="submit"
+							disabled={action.pending}
+						>
+							{action.pending
+								? 'Searching...'
+								: 'Search weather based on your location'}
+						</button>
+					</Form>
+				</section>
+				<Switch>
+					<Match when={action.result}>
+						<WeatherResult result={action.result} />
+					</Match>
+					<Match when={action.error}>
+						<p class="c-red-500">
+							An error ocurred while searching your weather. Please, try it later.
+						</p>
+					</Match>
+				</Switch>
 			</main>
 		</ErrorBoundary>
 	)
